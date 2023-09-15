5880c4556507f04e2ddea24ff4d87bf187186a2f
Satyam Kumar <ksatyam433@gmail.com>
2023-09-15 11:40:51 +0530

    page not found section deone

--- frontend/src/components/MainNav.jsx
+++ frontend/src/components/MainNav.jsx
@@ -18,52 +18,42 @@ function MainNav() {
                 >
                     <img
                         src={Logo}
-                        className='h-12 mr-3 pl-4'
+                        className='h-14 mr-3 pl-4'
                         alt='Flowbite Logo'
                     />
                 </NavLink>
-                <div className='flex md:order-2'>
+                <div className='flex md:order-2 '>
                     <NavLink to='/login'>
                         <button
                             type='button'
                             className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
                         >
-                            Login/SignUp
+                            Login
                         </button>
                     </NavLink>
                     <button
                         data-collapse-toggle='navbar-sticky'
                         type='button'
                         onClick={toggleMobileMenu}
-                        className='inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
+                        className='inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400  dark:focus:ring-gray-600'
                         aria-controls='navbar-sticky'
                         aria-expanded={mobileMenuOpen}
                     >
                         <span className='sr-only'>Open main menu</span>
-                        <svg
-                            className='w-5 h-5'
-                            aria-hidden='true'
-                            xmlns='http://www.w3.org/2000/svg'
-                            fill='none'
-                            viewBox='0 0 17 14'
-                        >
-                            <path
-                                stroke='currentColor'
-                                strokeLinecap='round'
-                                strokeLinejoin='round'
-                                strokeWidth='2'
-                                d='M1 1h15M1 7h15M1 13h15'
-                            />
-                        </svg>
+                        {mobileMenuOpen ? (
+                            <i className='bx bx-x text-3xl  '></i>
+                        ) : (
+                            <i className='bx bx-menu-alt-right text-2xl  '></i>
+                        )}
                     </button>
                 </div>
                 <div
                     className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${
                         mobileMenuOpen ? "block" : "hidden"
-                    }`}
+                    } `}
                     id='navbar-sticky'
                 >
-                    <ul className=' navBar_styles flex flex-col p-4 md:p-0 mt-4 font-medium   md:flex-row md:space-x-8 md:mt-0 md:border-0   '>
+                    <ul className=' navBar_styles flex flex-col p-4 md:p-0 mt-4 font-medium   md:flex-row md:space-x-8 md:mt-0 md:border-0 transition-all duration-500 ease-in-out '>
                         <li>
                             <NavLink
                                 to='/'
