6ff615c803ad88a882842336f46d23a3c0586c5c
Satyam Kumar <ksatyam433@gmail.com>
2023-11-11 23:42:13 +0530

    add: exercise Image

--- client/src/pages/Users/Exercises.jsx
+++ client/src/pages/Users/Exercises.jsx
@@ -3,6 +3,19 @@ function Exercises() {
     return (
         <>
             <UserNavbar />
+            <section className=' md:p-16'>
+                <div className=' w-full flex justify-center py-6'>
+                    <ul className='flex gap-x-8 md:gap-x-16'>
+                        <li className=' cursor-pointer'>All</li>
+                        <li className=' cursor-pointer'>Dumb bell</li>
+                        <li className=' cursor-pointer'>No Equipment</li>
+                        <li className=' cursor-pointer'>Stretching</li>
+                    </ul>
+                </div>
+                <div className='grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 '>
+                    <div className=''></div>
+                </div>
+            </section>
         </>
     );
 }
