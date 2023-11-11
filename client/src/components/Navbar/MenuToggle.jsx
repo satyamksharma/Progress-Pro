a25cd96cd861dd552066d4f5c4108c8efc5de905
Satyam Kumar <ksatyam433@gmail.com>
2023-11-11 18:29:55 +0530

    fix: minor fix

--- client/src/components/Navbar/MenuToggle.jsx
+++ client/src/components/Navbar/MenuToggle.jsx
@@ -1,46 +1,46 @@
-import { motion } from 'framer-motion';
+// import { motion } from 'framer-motion';
 
-const Path = (props) => (
-    <motion.path
-        fill='transparent'
-        strokeWidth='3'
-        stroke='hsl(0, 0%, 99.2156862745098%)'
-        strokeLinecap='round'
-        {...props}
-    />
-);
+// const Path = (props) => (
+//     <motion.path
+//         fill='transparent'
+//         strokeWidth='3'
+//         stroke='hsl(0, 0%, 99.2156862745098%)'
+//         strokeLinecap='round'
+//         {...props}
+//     />
+// );
 
-export const MenuToggle = ({ toggle }) => (
-    <button
-        className=' outline-none select-none border-none cursor-pointer absolute top-[0.1rem] left-5 w-16 h-16 bg-transparent rounded-full  '
-        onClick={toggle}
-    >
-        <svg
-            className='text-red-500 dark:text-white'
-            width='23'
-            height='23'
-            viewBox='0 0 23 23'
-        >
-            <Path
-                variants={{
-                    closed: { d: 'M 2 2.5 L 20 2.5' },
-                    open: { d: 'M 3 16.5 L 17 2.5' },
-                }}
-            />
-            <Path
-                d='M 2 9.423 L 20 9.423'
-                variants={{
-                    closed: { opacity: 1 },
-                    open: { opacity: 0 },
-                }}
-                transition={{ duration: 0.1 }}
-            />
-            <Path
-                variants={{
-                    closed: { d: 'M 2 16.346 L 20 16.346' },
-                    open: { d: 'M 3 2.5 L 17 16.346' },
-                }}
-            />
-        </svg>
-    </button>
-);
+// export const MenuToggle = ({ toggle }) => (
+//     <button
+//         className=' outline-none select-none border-none cursor-pointer absolute top-[0.1rem] left-5 w-16 h-16 bg-transparent rounded-full  '
+//         onClick={toggle}
+//     >
+//         <svg
+//             className='text-red-500 dark:text-white'
+//             width='23'
+//             height='23'
+//             viewBox='0 0 23 23'
+//         >
+//             <Path
+//                 variants={{
+//                     closed: { d: 'M 2 2.5 L 20 2.5' },
+//                     open: { d: 'M 3 16.5 L 17 2.5' },
+//                 }}
+//             />
+//             <Path
+//                 d='M 2 9.423 L 20 9.423'
+//                 variants={{
+//                     closed: { opacity: 1 },
+//                     open: { opacity: 0 },
+//                 }}
+//                 transition={{ duration: 0.1 }}
+//             />
+//             <Path
+//                 variants={{
+//                     closed: { d: 'M 2 16.346 L 20 16.346' },
+//                     open: { d: 'M 3 2.5 L 17 16.346' },
+//                 }}
+//             />
+//         </svg>
+//     </button>
+// );
