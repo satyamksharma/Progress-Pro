a2e06185bd00adbfbbbd7fb8ffc0242c673899ed
Satyam Kumar <ksatyam433@gmail.com>
2023-11-21 21:59:24 +0530

    add: encryption and jwt

--- client/src/pages/Users/Workout.jsx
+++ client/src/pages/Users/Workout.jsx
@@ -9,14 +9,19 @@ function Progress() {
             <section className='py-16 md:p-16 text-center'>
                 <h1 className='text-4xl font-semibold text-primary-light-200 '>Workout Plans</h1>
                 <p className='text-base font-sans text-gray-700'>Your Custom plans</p>
-                <div className=' mt-16'>
+                <div className=' mt-12'>
+                    <div className='my-4 w-full flex justify-end '>
+                        <button className='border border-primary-light-100 rounded-md p-2 px-4 hover:bg-primary-light-100 hover:text-bg-light text-lg'>
+                            Add a new workout
+                        </button>
+                    </div>
                     <div className=' w-full flex justify-between p-4 bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400 rounded-lg shadow-md hover:shadow-xl'>
                         <div className=' flex flex-col justify-start items-start'>
                             <h3 className=' text-2xl font-sans font-semibold'>Workout Plan 1</h3>
                             <p className=''>10 Exercises</p>
                         </div>
                         <div className='flex flex-col  items-end'>
-                            <div className='flex gap-x-4 text-xl mb-4  items-center justify-center '>
+                            <div className='flex gap-x-6 text-xl mb-4  items-center justify-center '>
                                 <button
                                     className='hover:scale-110 text-2xl'
                                     title='Edit Workout Plan'
@@ -31,7 +36,12 @@ function Progress() {
                                 </button>
                             </div>
                             <div className='m-4 bg-primary-light-100 p-2 px-3 rounded-lg'>
-                                <button title='Start Workout'>Start Workout</button>
+                                <button
+                                    title='Start Workout'
+                                    className='text-xl'
+                                >
+                                    Start Workout
+                                </button>
                             </div>
                         </div>
                     </div>
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,6 @@
-const { v4: uuid } = require('uuid');
 const { validationResult } = require('express-validator');
-
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const HttpError = require('../models/http-error');
 
@@ -27,10 +27,18 @@ const signup = async (req, res, next) => {
         return next(error);
     }
 
+    let hashedPassword;
+    try {
+        hashedPassword = await bcrypt.hash(password, 12);
+    } catch (err) {
+        const error = new HttpError('Could not create user, please try again.', 500);
+        return next(error);
+    }
+
     const createdUser = new User({
         name,
         email,
-        password,
+        password: hashedPassword,
         weight,
         age,
         height,
@@ -44,7 +52,19 @@ const signup = async (req, res, next) => {
         return next(error);
     }
 
-    res.status(201).json({ user: createdUser.toObject({ getters: true }) });
+    let token;
+    try {
+        token = jwt.sign(
+            { userId: createdUser.id, email: createdUser.email },
+            'supersecret_dont_share',
+            { expiresIn: '1h' }
+        );
+    } catch (err) {
+        const error = new HttpError('Signing up failed, please try again later.', 500);
+        return next(error);
+    }
+
+    res.status(201).json({ userId: createdUser.id, email: createdUser.email, token: token });
 };
 
 //Login function for user
@@ -56,16 +76,38 @@ const login = async (req, res, next) => {
     try {
         existingUser = await User.findOne({ email: email });
     } catch (err) {
-        const error = new HttpError('Logging in   failed, please try again later.', 500);
+        const error = new HttpError('Logging in failed, please try again later.', 500);
         return next(error);
     }
 
-    if (!existingUser || existingUser.password !== password) {
+    if (!existingUser) {
         const error = new HttpError('Invalid credentials, could not log you in.', 401);
         return next(error);
     }
 
-    res.json({ message: 'Logged in!' });
+    let isValidPassword = false;
+    try {
+        isValidPassword = await bcrypt.compare(password, existingUser.password);
+    } catch (err) {
+        const error = new HttpError(
+            'Could not log you in, please check your credentials and try again.',
+            500
+        );
+        return next(error);
+    }
+    let token;
+    try {
+        token = jwt.sign(
+            { userId: existingUser.id, email: existingUser.email },
+            'supersecret_dont_share',
+            { expiresIn: '1h' }
+        );
+    } catch (err) {
+        const error = new HttpError('Logging in failed, please try again later.', 500);
+        return next(error);
+    }
+
+    res.json({ userId: existingUser.id, email: existingUser.email, token: token });
 };
 
 exports.signup = signup;
